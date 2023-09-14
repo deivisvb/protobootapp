849ec416ce33598c486f24d84517a8f3a48bf8b1
Deivis Vargas <tech@cparking.com.co>
2023-09-14 18:54:26 -0500

    Update Calculadora.java

--- src/main/java/co/edu/eafit/protobootapp/Calculadora.java
+++ src/main/java/co/edu/eafit/protobootapp/Calculadora.java
@@ -8,7 +8,7 @@ public class Calculadora {
      * @return 
      */
     public Double cuadrado(Double numero) {
-        return numero + numero;
+        return numero * numero;
     }
     
     /**
