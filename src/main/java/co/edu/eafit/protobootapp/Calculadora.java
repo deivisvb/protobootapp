e591c48aa9309b1d7ccc4e109a7328cf4edcfea2
Deivis Vargas <tech@cparking.com.co>
2023-09-14 18:35:26 -0500

    Update Calculadora.java

--- src/main/java/co/edu/eafit/protobootapp/Calculadora.java
+++ src/main/java/co/edu/eafit/protobootapp/Calculadora.java
@@ -2,11 +2,20 @@ package co.edu.eafit.protobootapp;
 
 //This is a simple calculator class.
 public class Calculadora {
-
+    /**
+     * 
+     * @param numero
+     * @return 
+     */
     public Double cuadrado(Double numero) {
         return numero * numero;
     }
     
+    /**
+     * 
+     * @param numero
+     * @return 
+     */
     public Double cubo(Double numero) {
         return numero * numero * numero;
     }
